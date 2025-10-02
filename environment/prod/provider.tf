358f182180c9801100ee720e9cc96b82fb302661
Brijendra <brijendra@Brijendras-MacBook-Air.local>
2025-10-03 00:14:34 +0530

    Add Resource

--- environment/prod/provider.tf
+++ environment/prod/provider.tf
@@ -4,16 +4,17 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "4.45.0"
     }
-  }
-   backend "azurerm" {
+ 
+}
+ backend "azurerm" {
     resource_group_name  = "rg-brij"
     storage_account_name = "lofrestorage"
     container_name       = "lofercontainer"
-    key                  = "preprod.terraform.tfstate"
+    key                  = "prod.terraform.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
   subscription_id = "d73ab938-2a60-42e2-87cd-9362d4e73029"
-} 
+}
